0b231aa615c3fde3acf94b35a5ace06417bfcf6c
Gal Dadon <93877165+GaGex1222@users.noreply.github.com>
2025-02-08 23:17:25 +0200

    changed api key because accidentally leaked it lol

--- fireBaseConfig.ts
+++ fireBaseConfig.ts
@@ -1,13 +1,13 @@
 
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyApxg5Uv682RiuNtkmEv7E0ZLr2AXHjIRg",
-  authDomain: "py-start-44146.firebaseapp.com",
-  projectId: "py-start-44146",
-  storageBucket: "py-start-44146.firebasestorage.app",
-  messagingSenderId: "190681663488",
-  appId: "1:190681663488:web:b0eaf8d9f2a88a90f3f0e5",
-  measurementId: "G-04DMH5X7TQ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "py-start-51051.firebaseapp.com",
+  projectId: "py-start-51051",
+  storageBucket: "py-start-51051.firebasestorage.app",
+  messagingSenderId: "944177829955",
+  appId: "1:944177829955:web:cd8c9bd80f9af61faa5cf6",
+  measurementId: "G-H9FT2H34JK"
 };
 
 export const app = initializeApp(firebaseConfig);
